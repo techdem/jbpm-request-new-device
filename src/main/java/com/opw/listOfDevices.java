dbb7396ad52e84b67c77b54aa747439963d42d45
tudor <>
2019-08-28 16:29:12 +0100

    changed list of devices to booleans {/src/main/java/com/opw/listOfDevices.java}

--- src/main/java/com/opw/listOfDevices.java
+++ src/main/java/com/opw/listOfDevices.java
@@ -8,44 +8,12 @@ public class listOfDevices implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-    private java.lang.String broadband;
-    private java.lang.String camera;
-	private java.lang.String mobile;
-	private java.lang.String tablet;
+	private Boolean broadband;
+	private Boolean camera;
+	private Boolean mobile;
+	private Boolean tablet;
 
 	public listOfDevices() {
-	}
-
-	public void setBroadband(java.lang.String broadband) {
-		this.broadband = broadband;
-	}
-
-	public java.lang.String getBroadband() {
-		return this.broadband;
-	}
-
-    public void setCamera(java.lang.String camera) {
-		this.camera = camera;
-	}
-
-	public java.lang.String getCamera() {
-		return this.camera;
-	}
-
-    public void setMobile(java.lang.String mobile) {
-		this.mobile = mobile;
-	}
-
-	public java.lang.String getMobile() {
-		return this.mobile;
-	}
-
-    public void setTablet(java.lang.String tablet) {
-		this.tablet = tablet;
-	}
-
-	public java.lang.String getTablet() {
-		return this.tablet;
 	}
 
 	public listOfDevices(java.lang.String mobile, java.lang.String broadband,
@@ -54,6 +22,46 @@ public class listOfDevices implements java.io.Serializable {
 		this.broadband = broadband;
 		this.tablet = tablet;
 		this.camera = camera;
+	}
+
+	public java.lang.Boolean getBroadband() {
+		return this.broadband;
+	}
+
+	public void setBroadband(java.lang.Boolean broadband) {
+		this.broadband = broadband;
+	}
+
+	public java.lang.Boolean getCamera() {
+		return this.camera;
+	}
+
+	public void setCamera(java.lang.Boolean camera) {
+		this.camera = camera;
+	}
+
+	public java.lang.Boolean getMobile() {
+		return this.mobile;
+	}
+
+	public void setMobile(java.lang.Boolean mobile) {
+		this.mobile = mobile;
+	}
+
+	public java.lang.Boolean getTablet() {
+		return this.tablet;
+	}
+
+	public void setTablet(java.lang.Boolean tablet) {
+		this.tablet = tablet;
+	}
+
+	public listOfDevices(java.lang.Boolean broadband, java.lang.Boolean camera,
+			java.lang.Boolean mobile, java.lang.Boolean tablet) {
+		this.broadband = broadband;
+		this.camera = camera;
+		this.mobile = mobile;
+		this.tablet = tablet;
 	}
 
 }
